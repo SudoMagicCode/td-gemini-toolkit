d443313ddf6e6b7b735ec8153cd288825010a5c3
Matthew Ragan <raganmd@gmail.com>
2026-04-14 12:00:11 -0700

    adds doc strings

--- TouchDesigner/td-python/apiKeyActions.py
+++ TouchDesigner/td-python/apiKeyActions.py
@@ -2,16 +2,23 @@ import smOpUtils
 
 
 def Addapikey(tdPar: Par) -> None:
+    '''Parameter function used to add API key from TD Par interface
+    '''
     print("adding api key")
     smOpUtils.get_api_key()
 
 
 def Clearapikey(tdPar: Par) -> None:
+    '''Parameter function used to remove API key from TD Par interface
+    '''
     print("removing api key")
     parent.geminiCOMP.unstore('gemini_apiKey')
 
 
 def Distributeapikey(tdPar: Par) -> None:
+    '''Parameter function used to update API key on all ops with the 
+    tag 'gemini' from TD Par interface
+    '''
     print("distributing api key")
     api_key = parent.geminiCOMP.fetch('gemini_apiKey')
 

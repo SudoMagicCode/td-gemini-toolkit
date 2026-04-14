b7b81c87b6b4509978c0492baf9c2c1c7ea3f608
Matthew Ragan <raganmd@gmail.com>
2026-04-14 11:10:52 -0700

    Makes clones out of local and AI Engine

--- TouchDesigner/td-python/RequestBroker/requestBroker.py
+++ TouchDesigner/td-python/RequestBroker/requestBroker.py
@@ -3,72 +3,76 @@ from abc import ABC, abstractmethod
 
 from requestObjectBase import RequestObjectBase
 
+
 class RequestBroker:
-	"""RequestBroker handles all requests to the API and brokers their responses through callbacks
-	"""
-	def __init__(self, _thisOp: OP):
-		self._thisOp = _thisOp
-		self._apiKey = parent.geminiCOMP.par.Apikey.eval()
-		self._webclientDat:webclientDAT = self._thisOp.op("webclient1")
-		self._requestLookup:Dict[int, RequestObjectBase] = {}
-		pass
+    """RequestBroker handles all requests to the API and brokers their responses through callbacks
+    """
 
-	def _makeRequest(self, requestObject:RequestObjectBase, url:str, method:str, header=None):
-		'''internal request initializer, this method will create the request on the webclientDat'''
-		# TODO: create request against the webclient DAT
-		id = self._webclientDat.request(url, method, header=header, data=requestObject.input())
-		self._requestLookup[id] = requestObject
-		pass
+    def __init__(self, _thisOp: OP):
+        self._thisOp = _thisOp
+        self._apiKey = parent.geminiCOMP.fetch('gemini_apiKey')
+        self._webclientDat: webclientDAT = self._thisOp.op("webclient1")
+        self._requestLookup: Dict[int, RequestObjectBase] = {}
+        pass
 
-	def _completeRequest(self, statusCode: Dict[str, Any], headerDict: Dict[str, str], data: bytes, id: int):
-		'''internal request completer, this method will resolve the RequestObjectBase with data from the webclientDat'''
-		# find the request object
-		requestObject = self._requestLookup[id]
-		
-		# check if an object was found
-		if requestObject is None:
-			del self._requestLookup[id]
-			return
+    def _makeRequest(self, requestObject: RequestObjectBase, url: str, method: str, header=None):
+        '''internal request initializer, this method will create the request on the webclientDat'''
+        # TODO: create request against the webclient DAT
+        id = self._webclientDat.request(
+            url, method, header=header, data=requestObject.input())
+        self._requestLookup[id] = requestObject
+        pass
 
-		try:
-			# attempt to resolve the request object
-			requestObject._resolve(statusCode, headerDict, data)
-		except Exception as e:
-			# something went wrong in the resolving code...
-			print(e)
-		
-		# delete the request object from lookup
-		del self._requestLookup[id]
-		return
+    def _completeRequest(self, statusCode: Dict[str, Any], headerDict: Dict[str, str], data: bytes, id: int):
+        '''internal request completer, this method will resolve the RequestObjectBase with data from the webclientDat'''
+        # find the request object
+        requestObject = self._requestLookup[id]
 
-	def _completeRequestAsError(self, id: int, error: Exception):
-		# find the request object
-		requestObject = self._requestLookup[id]
-		
-		# check if an object was found
-		if requestObject is None:
-			del self._requestLookup[id]
-			return
-		
-		try:
-			# attempt to resolve the request object
-			requestObject._error(error)
-		except Exception as e:
-			# something went wrong in the resolving code...
-			print(e)
-			
-		# delete the request object from lookup
-		del self._requestLookup[id]
+        # check if an object was found
+        if requestObject is None:
+            del self._requestLookup[id]
+            return
 
-	def MakeRequest(self, requestObject:RequestObjectBase):
-		requestObject._header["x-goog-api-key"] = self._apiKey
-		self._makeRequest(requestObject, url=requestObject.url(), method=requestObject.method(), header=requestObject.header())
-		pass
+        try:
+            # attempt to resolve the request object
+            requestObject._resolve(statusCode, headerDict, data)
+        except Exception as e:
+            # something went wrong in the resolving code...
+            print(e)
 
-	def CompleteRequest(self, statusCode: Dict[str, Any], headerDict: Dict[str, str], data: bytes, id: int):
-		self._completeRequest(statusCode, headerDict, data, id)
-		pass
+        # delete the request object from lookup
+        del self._requestLookup[id]
+        return
 
-	def CompleteRequestAsError(self, id: int, error: Exception):
-		self._completeRequestAsError(id, error)
-		pass
+    def _completeRequestAsError(self, id: int, error: Exception):
+        # find the request object
+        requestObject = self._requestLookup[id]
+
+        # check if an object was found
+        if requestObject is None:
+            del self._requestLookup[id]
+            return
+
+        try:
+            # attempt to resolve the request object
+            requestObject._error(error)
+        except Exception as e:
+            # something went wrong in the resolving code...
+            print(e)
+
+        # delete the request object from lookup
+        del self._requestLookup[id]
+
+    def MakeRequest(self, requestObject: RequestObjectBase):
+        requestObject._header["x-goog-api-key"] = self._apiKey
+        self._makeRequest(requestObject, url=requestObject.url(
+        ), method=requestObject.method(), header=requestObject.header())
+        pass
+
+    def CompleteRequest(self, statusCode: Dict[str, Any], headerDict: Dict[str, str], data: bytes, id: int):
+        self._completeRequest(statusCode, headerDict, data, id)
+        pass
+
+    def CompleteRequestAsError(self, id: int, error: Exception):
+        self._completeRequestAsError(id, error)
+        pass

a53a287a5d40b80ecc0852eb8836bff6b041a4a5
Matthew Ragan <raganmd@gmail.com>
2026-04-15 14:21:08 -0700

    Updates logging
    Adds script error to component when API key is missing

--- TouchDesigner/td-python/RequestBroker/requestBroker.py
+++ TouchDesigner/td-python/RequestBroker/requestBroker.py
@@ -26,6 +26,7 @@ class RequestBroker:
         if parent.geminiCOMP.fetch('gemini_apiKey', None) == None:
             msg_formatter(
                 "Missing api key, please ensure you've added an API key to your component")
+            self._thisOp.parent().addScriptError("Missing Gemini API Key")
             raise ValueError("Missing api key")
         else:
             return parent.geminiCOMP.fetch('gemini_apiKey')

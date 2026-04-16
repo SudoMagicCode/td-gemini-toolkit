266a452fbb49b79c9ae3de9679176293f0f266d6
Ian Shelanskey <ishelanskey@gmail.com>
2026-04-16 13:16:02 -0700

    adds generation config

--- TouchDesigner/td-python/geminiObjects/__init__.py
+++ TouchDesigner/td-python/geminiObjects/__init__.py
@@ -2,6 +2,8 @@ from contents import *
 from models import Models
 from operations import RpcOperations
 from vars import *
+from generationConfig import *
+
 import operatorAdaptors
 
 # adapts touchdesigner operators to gemini objects

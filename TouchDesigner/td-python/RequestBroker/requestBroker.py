10abbe6a3679d6fa3f1c09914887ed722c10dc51
Ian Shelanskey <ishelanskey@gmail.com>
2026-04-16 09:08:03 -0700

    fixes weird line breaks

--- TouchDesigner/td-python/RequestBroker/requestBroker.py
+++ TouchDesigner/td-python/RequestBroker/requestBroker.py
@@ -13,8 +13,7 @@ class RequestBroker:
     def __init__(self, _thisOp: OP):
         self.my_id = uuid.uuid5(uuid.NAMESPACE_OID, _thisOp.path)
         self._thisOp = _thisOp
-        msg_formatter(
-            f"initializing request broker {str(self.my_id)} at {self._thisOp.path}")
+        msg_formatter(f"initializing request broker {str(self.my_id)} at {self._thisOp.path}")
         self._webclientDat: webclientDAT = self._thisOp.op("webclient1")
         self._requestLookup: Dict[int, RequestObjectBase] = {}
         self._awaiting_response = tdu.Dependency(False)
@@ -24,8 +23,7 @@ class RequestBroker:
     @property
     def _apiKey(self) -> str:
         if parent.geminiCOMP.fetch('gemini_apiKey', None) == None:
-            msg_formatter(
-                "Missing api key, please ensure you've added an API key to your component")
+            msg_formatter("Missing api key, please ensure you've added an API key to your component")
             self._thisOp.parent().addScriptError("Missing Gemini API Key")
             raise ValueError("Missing api key")
         else:
@@ -33,8 +31,7 @@ class RequestBroker:
 
     def _makeRequest(self, requestObject: RequestObjectBase, url: str, method: str, header=None):
         '''internal request initializer, this method will create the request on the webclientDat'''
-        id = self._webclientDat.request(
-            url, method, header=header, data=requestObject.input())
+        id = self._webclientDat.request(url, method, header=header, data=requestObject.input())
         self._requestLookup[id] = requestObject
         msg_formatter(f"{self._thisOp.path} broker making request")
         self._awaiting_response.val = True
@@ -45,8 +42,7 @@ class RequestBroker:
         # find the request object
         if id not in self._requestLookup:
             # there has been some issue with the request map resetting
-            msg_formatter(
-                f"request {id} not found in broker {str(self.my_id)} at {self._thisOp.path}")
+            msg_formatter(f"request {id} not found in broker {str(self.my_id)} at {self._thisOp.path}")
             pass
 
         requestObject = self._requestLookup[id]
@@ -62,8 +58,7 @@ class RequestBroker:
             self._awaiting_response.val = False
         except Exception as e:
             # something went wrong in the resolving code...
-            msg_formatter(
-                f"{str(self.my_id)} at {self._thisOp.path} raised Exception for request {id}:{e}")
+            msg_formatter(f"{str(self.my_id)} at {self._thisOp.path} raised Exception for request {id}:{e}")
 
         # delete the request object from lookup
         del self._requestLookup[id]
@@ -73,8 +68,7 @@ class RequestBroker:
         # find the request object
         if id not in self._requestLookup:
             # there has been some issue with the request map resetting
-            msg_formatter(
-                f"request {id} not found in broker {str(self.my_id)} at {self._thisOp.path}")
+            msg_formatter(f"request {id} not found in broker {str(self.my_id)} at {self._thisOp.path}")
             pass
 
         requestObject = self._requestLookup[id]
@@ -90,16 +84,14 @@ class RequestBroker:
 
         except Exception as e:
             # something went wrong in the resolving code...
-            msg_formatter(
-                f"{str(self.my_id)} at {self._thisOp.path} raised Exception for request {id}:{e}")
+            msg_formatter(f"{str(self.my_id)} at {self._thisOp.path} raised Exception for request {id}:{e}")
 
         # delete the request object from lookup
         del self._requestLookup[id]
 
     def MakeRequest(self, requestObject: RequestObjectBase):
         requestObject._header["x-goog-api-key"] = self._apiKey
-        self._makeRequest(requestObject, url=requestObject.url(
-        ), method=requestObject.method(), header=requestObject.header())
+        self._makeRequest(requestObject, url=requestObject.url(), method=requestObject.method(), header=requestObject.header())
         pass
 
     def CompleteRequest(self, statusCode: Dict[str, Any], headerDict: Dict[str, str], data: bytes, id: int):

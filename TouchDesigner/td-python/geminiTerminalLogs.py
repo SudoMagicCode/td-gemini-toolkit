3a8261fa19fc4a1e360373de993175e8acd2715d
Matthew Ragan <raganmd@gmail.com>
2026-04-17 17:02:01 -0700

    Guards against addition case for api key check

--- TouchDesigner/td-python/geminiTerminalLogs.py
+++ TouchDesigner/td-python/geminiTerminalLogs.py
@@ -5,13 +5,13 @@ from datetime import datetime
 def get_pretty_timestamp():
     """Returns the current time formatted as YYYY-MM-DD | HH:MM:SS"""
     now = datetime.now()
-    return now.strftime("%Y-%m-%d | %H:%M:%S")
+    return now.strftime("%Y-%m-%d %H:%M:%S |")
 
 
 def msg_formatter(msg: str, indent: int = 0, displayInTextport: bool = True) -> str:
-    '''Prints and returns a string with a time stamp and indent - suitable for logs
+    """Prints and returns a string with a time stamp and indent - suitable for logs
     and the textport
-    '''
+    """
     indentText = f"{'--' * indent}> "
     formattedMsg = f"{get_pretty_timestamp()} ❇️ {indentText if indent>0 else ''}{msg}"
     if displayInTextport:

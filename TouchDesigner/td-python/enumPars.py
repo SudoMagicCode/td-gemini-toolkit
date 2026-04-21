e1cc23aecf1b10c25eafceccb46b4c04015da718
Matthew Ragan <raganmd@gmail.com>
2026-04-21 11:20:45 -0700

    Updates Enum Pars

--- TouchDesigner/td-python/enumPars.py
+++ TouchDesigner/td-python/enumPars.py
@@ -4,8 +4,27 @@ from smOpUtils import ModelAsPar
 
 
 class TextModels(Enum):
+    """
+    Gemini Text Models
+    """
+
     GEMINI_3_1 = ModelAsPar(Models.GEMINI_3_1_PRO_PREVIEW, "Gemini 3.1")
     GEMINI_3_1_FLASH = ModelAsPar(Models.GEMINI_3_FLASH_PREVIEW, "Gemini 3 Flash")
     GEMINI_3_1_FLASH_LITE = ModelAsPar(
         Models.GEMINI_3_1_FLASH_LITE_PREVIEW, "Gemini 3.1 Flash Lite"
     )
+
+
+class VeoModels(Enum):
+    """
+    VEO Models
+    """
+
+    VEO_3_1_GENERATE_PREVIEW = ModelAsPar(Models.VEO_3_1_GENERATE_PREVIEW, "VEO 3.1")
+    VEO_3_1_FAST_GENERATE_PREVIEW = ModelAsPar(
+        Models.VEO_3_1_FAST_GENERATE_PREVIEW, "VEO 3.1 Fast"
+    )
+    VEO_3_1_LITE_GENERATE_PREVIEW = ModelAsPar(
+        Models.VEO_3_1_LITE_GENERATE_PREVIEW, "VEO 3.1 Lite"
+    )
+    VEO_3_0_GENERATE_PREVIEW = ModelAsPar(Models.VEO_3_0_GENERATE_PREVIEW, "VEO 3.0")

60e44164fa91b75c098de6b4b1d505f5c7344dba
Ian Shelanskey <ishelanskey@gmail.com>
2026-04-23 13:03:06 -0700

    adds enterprise models, updates endpoint function to accept base_url

--- TouchDesigner/td-python/geminiObjects/vars.py
+++ TouchDesigner/td-python/geminiObjects/vars.py
@@ -1,12 +1,19 @@
 import models
 import operations
 
+
 GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/v1beta"
 
 
-def CreateEndpoint(model: models.Models, operation: operations.RpcOperations) -> str:
+def CreateEndpoint(
+    model: models.Models, operation: operations.RpcOperations, base_url: str = None
+) -> str:
+    if base_url is not None:
+        return f"{base_url}/{model}:{operation}"
     return f"{GEMINI_BASE_URL}/{model}:{operation}"
 
 
-def CreateRawEndpoint(name: str) -> str:
+def CreateRawEndpoint(name: str, base_url: str = None) -> str:
+    if base_url is not None:
+        return f"{base_url}/{name}"
     return f"{GEMINI_BASE_URL}/{name}"

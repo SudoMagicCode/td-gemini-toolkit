0143a684bef87a9f233946ef23c9db90d7b7cecb
Ian Shelanskey <ishelanskey@gmail.com>
2026-04-23 17:31:34 -0700

    update enum pars with all of the new models

--- TouchDesigner/td-python/enumPars.py
+++ TouchDesigner/td-python/enumPars.py
@@ -2,17 +2,17 @@ from enum import Enum
 from geminiObjects import Models
 from smOpUtils import ModelAsPar
 
-
+# fmt: off
 class TextModels(Enum):
     """
     Gemini Text Models
     """
 
-    GEMINI_3_1 = ModelAsPar(Models.GEMINI_3_1_PRO_PREVIEW, "Gemini 3.1")
-    GEMINI_3_1_FLASH = ModelAsPar(Models.GEMINI_3_FLASH_PREVIEW, "Gemini 3 Flash")
-    GEMINI_3_1_FLASH_LITE = ModelAsPar(
-        Models.GEMINI_3_1_FLASH_LITE_PREVIEW, "Gemini 3.1 Flash Lite"
-    )
+    GEMINI_2_5_FLASH        = ModelAsPar(Models.GEMINI_2_5_FLASH, "Gemini 2.5 Flash")
+    GEMINI_2_5_PRO          = ModelAsPar(Models.GEMINI_2_5_PRO, "Gemini 2.5 Pro")
+    GEMINI_3_FLASH_PREVIEW  = ModelAsPar(Models.GEMINI_3_FLASH_PREVIEW, "Gemini 3 Flash Preview")
+    GEMINI_3_1_PRO_PREVIEW  = ModelAsPar(Models.GEMINI_3_1_PRO_PREVIEW, "Gemini 3.1 Pro Preview")
+    GEMINI_3_1_FLASH_LITE   = ModelAsPar(Models.GEMINI_3_1_FLASH_LITE_PREVIEW, "Gemini 3.1 Flash Lite Preview")
 
 
 class ImageModels(Enum):
@@ -20,12 +20,18 @@ class ImageModels(Enum):
     Gemini Image Models
     """
 
-    GEMINI_3_0_PRO_IMAGE_PREVIEW = ModelAsPar(
-        Models.GEMINI_3_0_PRO_IMAGE_PREVIEW, "Nano Banana Pro"
-    )
-    GEMINI_3_1_FLASH_IMAGE_PREVIEW = ModelAsPar(
-        Models.GEMINI_3_1_FLASH_IMAGE_PREVIEW, "Nano Banana 2"
-    )
+    # Vanity - these models are duplicates of other models with vanity marketing names
+    NANO_BANANA_PRO     = ModelAsPar(Models.GEMINI_3_PRO_IMAGE_PREVIEW, "Nano Banana Pro")
+    NANO_BANANA_2       = ModelAsPar(Models.GEMINI_3_1_FLASH_IMAGE_PREVIEW, "Nano Banana 2")
+
+    # Actual
+    GEMINI_3_0_PRO_IMAGE_PREVIEW    = ModelAsPar(Models.GEMINI_3_PRO_IMAGE_PREVIEW, "Gemini 3.0 Pro Image Preview")
+    GEMINI_3_1_PRO_IMAGE_PREVIEW    = ModelAsPar(Models.GEMINI_3_PRO_IMAGE_PREVIEW, "Gemini 3.1 Pro Image Preview")
+    GEMINI_3_1_FLASH_IMAGE_PREVIEW  = ModelAsPar(Models.GEMINI_3_1_FLASH_IMAGE_PREVIEW, "Gemini 3.1 Flash Image Preview")
+    GEMINI_2_5_FLASH_IMAGE          = ModelAsPar(Models.GEMINI_2_5_FLASH_IMAGE, "Gemini 2.5 Flash Image")
+    IMAGEN_3_0_GENERATE_002         = ModelAsPar(Models.IMAGEN_3_0_GENERATE_002, "Imagen 3.0 Generate 002")
+    IMAGEN_3_0_FAST_GENERATE_001    = ModelAsPar(Models.IMAGEN_3_0_FAST_GENERATE_001, "Imagen 3.0 Fast Generate 001")
+    IMAGEN_4_0_GENERATE_PREDICT     = ModelAsPar(Models.IMAGEN_4_0_GENERATE_PREDICT, "Imagen 4.0 Generate Predict")
 
 
 class AudioModels(Enum):
@@ -33,8 +39,21 @@ class AudioModels(Enum):
     Gemini Audio Models
     """
 
-    LYRIA_3_CLIP_PREVIEW = ModelAsPar(Models.LYRIA_3_CLIP_PREVIEW, "Lyria 3 Clip")
-    LYRIA_3_PRO_PREVIEW = ModelAsPar(Models.LYRIA_3_PRO_PREVIEW, "Lyria 3 Pro")
+    LYRIA_3_CLIP_PREVIEW    = ModelAsPar(Models.LYRIA_3_CLIP_PREVIEW, "Lyria 3 Clip")
+    LYRIA_3_PRO_PREVIEW     = ModelAsPar(Models.LYRIA_3_PRO_PREVIEW, "Lyria 3 Pro")
+    LYRIA_002               = ModelAsPar(Models.LYRIA_002, "Lyria 002")
+
+
+class TTSModels(Enum):
+    """
+    Gemini TTS Models
+    """
+
+    GEMINI_2_5_FLASH_PREVIEW_TTS    = ModelAsPar(Models.GEMINI_2_5_FLASH_TTS, "Gemini 2.5 Flash Preview TTS")
+    GEMINI_2_5_PRO_PREVIEW_TTS      = ModelAsPar(Models.GEMINI_2_5_PRO_PREVIEW_TTS, "Gemini 2.5 Pro Preview TTS")
+    GEMINI_2_5_FLASH_TTS            = ModelAsPar(Models.GEMINI_2_5_FLASH_TTS, "Gemini 2.5 Flash TTS")
+    GEMINI_2_5_PRO_TTS              = ModelAsPar(Models.GEMINI_2_5_PRO_TTS, "Gemini 2.5 Pro TTS")
+    GEMINI_3_1_PREVIEW_TTS          = ModelAsPar(Models.GEMINI_3_1_PREVIEW_TTS, "Gemini 3.1 Preview TTS")
 
 
 class VeoModels(Enum):
@@ -42,11 +61,14 @@ class VeoModels(Enum):
     VEO Video Models
     """
 
-    VEO_3_1_GENERATE_PREVIEW = ModelAsPar(Models.VEO_3_1_GENERATE_PREVIEW, "VEO 3.1")
-    VEO_3_1_FAST_GENERATE_PREVIEW = ModelAsPar(
-        Models.VEO_3_1_FAST_GENERATE_PREVIEW, "VEO 3.1 Fast"
-    )
-    VEO_3_1_LITE_GENERATE_PREVIEW = ModelAsPar(
-        Models.VEO_3_1_LITE_GENERATE_PREVIEW, "VEO 3.1 Lite"
-    )
-    VEO_3_0_GENERATE_PREVIEW = ModelAsPar(Models.VEO_3_0_GENERATE_PREVIEW, "VEO 3.0")
+    VEO_3_1_GENERATE_PREVIEW        = ModelAsPar(Models.VEO_3_1_GENERATE_PREVIEW, "VEO 3.1 Preview")
+    VEO_3_1_FAST_GENERATE_PREVIEW   = ModelAsPar(Models.VEO_3_1_FAST_GENERATE_PREVIEW, "VEO 3.1 Fast Preview")
+    VEO_3_1_LITE_GENERATE_PREVIEW   = ModelAsPar(Models.VEO_3_1_LITE_GENERATE_PREVIEW, "VEO 3.1 Lite Preview")
+    VEO_2_0_GENERATE_001            = ModelAsPar(Models.VEO_2_0_GENERATE_001, "VEO 2.0 Generate 001")
+    VEO_3_0_GENERATE_001            = ModelAsPar(Models.VEO_3_0_GENERATE_001, "VEO 3.0 Generate 001")
+    VEO_3_0_FAST_GENERATE_001       = ModelAsPar(Models.VEO_3_0_FAST_GENERATE_001, "VEO 3.0 Fast Generate 001")
+    VEO_3_1_GENERATE_001            = ModelAsPar(Models.VEO_3_1_GENERATE_001, "VEO 3.1 Generate 001")
+    VEO_3_1_FAST_GENERATE_001       = ModelAsPar(Models.VEO_3_1_FAST_GENERATE_001, "VEO 3.1 Fast Generate 001")
+    VEO_3_1_LITE_GENERATE_001       = ModelAsPar(Models.VEO_3_1_LITE_GENERATE_001, "VEO 3.1 Lite Generate 001")
+
+# fmt: on

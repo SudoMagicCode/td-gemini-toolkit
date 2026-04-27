26af395eacc9426a1dfe915f1ffbe7f0c4409ee3
Matthew Ragan <raganmd@gmail.com>
2026-04-26 22:34:15 -0700

    Config update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://sudomagiccode.github.io/td-gemini-tools/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/td-gemini-tools',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

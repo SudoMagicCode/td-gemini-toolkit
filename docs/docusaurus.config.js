6b485bc399a9ca7069c25c14b02576de787bc046
Matthew Ragan <raganmd@gmail.com>
2026-04-26 22:32:08 -0700

    Config updates

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sudomagiccode.github.io/td-gemini-tools/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/td-gemini-tools',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
